602b090bcded6b33f2d8e3c70ff60b58b170841b
vasylcherepushko <vasyl.cherepushko@yandex.ru>
2017-01-15 08:44:14 +0200

    Add files via upload

---
+++ to_camel_case.py
@@ -0,0 +1,8 @@
+def to_camel_case(text):
+    if not text:
+        return text
+    low = text[0].islower()
+    res = text.title().replace("-", "").replace("_", "")
+    if low:
+        return res[0].lower() + res[1:]
+    return res
