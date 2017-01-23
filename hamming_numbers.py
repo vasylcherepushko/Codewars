5ea76a9188e01688a628ddd8df2b298f97c1d7e9
vasylcherepushko <vasyl.cherepushko@yandex.ru>
2017-01-23 18:07:07 +0200

    Update hamming_numbers.py

--- hamming_numbers.py
+++ hamming_numbers.py
@@ -25,11 +25,11 @@ def hamming_alternative(n):
     expos = [0, 0, 0]
     hamms = [1]
 
-	for _ in range(1, n):
-        next_hamms = [bases[i] * hamms[expos[i]] for i in range(3)]
-        next_hamm = min(next_hamms)
-        hamms.append(next_hamm)
-        for i in range(3):
-            expos[i] += int(next_hamms[i] == next_hamm)
+    for _ in range(1, n):
+    next_hamms = [bases[i] * hamms[expos[i]] for i in range(3)]
+    next_hamm = min(next_hamms)
+    hamms.append(next_hamm)
+    for i in range(3):
+        expos[i] += int(next_hamms[i] == next_hamm)
 
-	return hamms[-1]
+    return hamms[-1]
