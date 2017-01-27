51e2013a8829686305461b896bd6fd5308a9e1f5
vasylcherepushko <vasyl.cherepushko@yandex.ru>
2017-01-27 07:42:08 +0200

    Add files via upload

---
+++ tortoise_racing.py
@@ -0,0 +1,8 @@
+# Link to this kata
+# https://www.codewars.com/kata/tortoise-racing
+
+def race(v1, v2, g):
+    if v1 >= v2:
+        return
+    seconds = int(g * 3600 / (v2 - v1))
+    return [seconds // 3600, seconds % 3600 // 60, seconds % 60]
