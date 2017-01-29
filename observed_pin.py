71de321ef4b5dbc7d93748877187411b44d1c2b5
vasylcherepushko <vasyl.cherepushko@yandex.ru>
2017-01-29 11:42:16 +0200

    Delete observed_pin.py

--- observed_pin.py
+++
@@ -1,9 +0,0 @@
-# Link to this kata
-# https://www.codewars.com/kata/the-observed-pin
-
-from itertools import product
-
-
-def get_pins(observed):
-    ADJACENTS = ('08', '124', '2135', '326', '4157', '52468', '6359', '748', '85790', '968')
-    return [''.join(p) for p in product(*[ADJACENTS[int(d)] for d in observed])]
