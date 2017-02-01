d1aaff8df4a58b2a2b1e5cf8859ac77679f1d4b3
vasylcherepushko <vasyl.cherepushko@yandex.ru>
2017-02-01 09:38:34 +0200

    Add files via upload

---
+++ multiples_of_3_and_5.py
@@ -0,0 +1,8 @@
+# Link to this kata
+# https://www.codewars.com/kata/multiples-of-3-and-5
+
+def solution(number):
+    multiples_3 = sum(range(3, number, 3))
+    multiples_5 = sum(range(5, number, 5))
+    multiples_15 = sum(range(15, number, 15))
+    return multiples_3 + multiples_5 - multiples_15
