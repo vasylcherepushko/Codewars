9e5f08ea427b0aa19cea89f015b326774483e2cb
vasylcherepushko <vasyl.cherepushko@yandex.ru>
2017-02-03 10:07:00 +0200

    Add files via upload

---
+++ counting_duplicates.py
@@ -0,0 +1,13 @@
+# Link to this kata
+# https://www.codewars.com/kata/counting-duplicates
+
+from collections import Counter
+
+
+def duplicate_count(text):
+    counter = Counter(text.lower())
+    res = 0    
+    for char in counter:
+        if counter[char] > 1:
+            res += 1
+    return res
