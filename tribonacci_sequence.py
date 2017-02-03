3fc8e8b59cfd9f51371fa08158522573804d9522
vasylcherepushko <vasyl.cherepushko@yandex.ru>
2017-02-03 11:38:41 +0200

    Add files via upload

--- tribonacci_sequence.py
+++ tribonacci_sequence.py
@@ -1,7 +1,8 @@
 # Link to this kata
 # https://www.codewars.com/kata/tribonacci-sequence
 
-def tribonacci(signature, n):
+def tribonacci(signature,n):
     res = signature[:n]
-    for i in range(n - 3): res.append(sum(res[-3:]))
+    for i in range(n-3):
+        res.append(res[-3] + res[-2] + res[-1])
     return res
