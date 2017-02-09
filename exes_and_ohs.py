c4bea4fb0922ea8146113719bb93705b4901ee42
vasylcherepushko <vasyl.cherepushko@yandex.ru>
2017-02-09 23:50:09 +0200

    Add files via upload

---
+++ exes_and_ohs.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/exes-and-ohs
+
+def xo(s):
+    diff = 0
+    for char in s.lower():
+        if char == 'x':
+            diff += 1
+        elif char =='o':
+            diff -= 1
+    return not diff
