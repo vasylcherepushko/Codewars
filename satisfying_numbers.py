b398dccd3949e14fe671c04ee606ecbf1ffe5dde
vasylcherepushko <vasyl.cherepushko@yandex.ru>
2017-02-23 10:30:28 +0200

    Add files via upload

---
+++ satisfying_numbers.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/satisfying-numbers
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def smallest(n):
+    res = 1
+    
+    for i in range(2, n + 1):
+        res *= i / gcd(i, res)
+      
+    return res
