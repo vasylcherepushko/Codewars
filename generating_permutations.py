ab551a0e95d9cd09c11d8b7f947125b192fb6902
vasylcherepushko <vasyl.cherepushko@yandex.ru>
2017-02-27 12:19:18 +0200

    Add files via upload

--- generating_permutations.py
+++ generating_permutations.py
@@ -23,5 +23,5 @@ def permutations(l):
   if len(l) <= 1: yield l
   else:
     for perm in permutations(l[1:]):
-      for i in xrange(len(perm)+1):
+      for i in range(len(perm)+1):
         yield perm[:i] + l[0:1] + perm[i:]
